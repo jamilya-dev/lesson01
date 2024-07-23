632988c2908a62c3bc371a05333d879c9fa4c8cf
jamilya-dev <omg.groups@yandex.ru>
2024-07-23 10:53:08 +0300

    error correction 2

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 console.log('Стоимость разработки сайта ' + screenPrice + '₽');
-let arr = screens.toLowerCase().toString().split("");
+let arr = screens.toLowerCase().toString().split(", ");
 console.log(arr);
 console.log(fullPrice * (rollback / 100));
 
