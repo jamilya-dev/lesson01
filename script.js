66f2133295d0c321230c1b3b81ea8e31384d005c
jamilya-dev <omg.groups@yandex.ru>
2024-07-24 21:16:10 +0300

    Исправление ошибок

--- script.js
+++ script.js
@@ -1,37 +1,13 @@
-let title = 'Название проекта';
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 300;
-let rollback = 99;
-let fullPrice = 100000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость разработки сайта ' + screenPrice + '₽');
-let arr = screens.toLowerCase().toString().split("");
-console.log(arr);
-console.log(fullPrice * (rollback / 100));
-
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать? пример: "Простые, Сложные, Интерактивные"');
-screenPrice = prompt('Сколько будет стоить данная работа?');
-adaptiveQuest = prompt('Нужен ли адаптив на сайте?');
-if (adaptiveQuest === 'Да' || adaptiveQuest === 'да') {
-  adaptive = true;
-} else if (adaptiveQuest === 'Нет' || adaptiveQuest === 'нет') {
-  adaptive = false;
-} else {
-  adaptive = 'Некорректное значение';
-};
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = prompt('Сколько это будет стоить?');
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать? пример: "Простые, Сложные, Интерактивные"');
+const screenPrice = prompt('Сколько будет стоить данная работа?');
+const answer = confirm("Нужен ли адаптив на сайте?")
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = prompt('Сколько это будет стоить?');
 fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-let servicePercentPrice = Math.ceil(fullPrice - rollback);
-console.log(servicePercentPrice);
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
 switch (true) {
   case fullPrice >= 30000:
     console.log('Даем скидку в 10%');
@@ -46,3 +22,4 @@ switch (true) {
     console.log('Что то пошло не так');
     break
 }
+console.log(servicePercentPrice);
