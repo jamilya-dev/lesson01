9b22d1ef86cfcdc7da104315285258c69c6eb03e
jamilya-dev <omg.groups@yandex.ru>
2024-07-25 22:01:51 +0300

    выполнено задание 4

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ let servicePrice2 = prompt('Сколько это будет стоить?');
 let rollback = 10
 let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
 let servicePercentPrice = Math.ceil(fullPrice - (rollback / 100));
+let allServicePrices;
+
 
 const showTypeOf = function (variable) {
-  console.log(variable, typeof variable);
+  return variable + typeof variable;
 }
 
-const retRollbackMessage = function (price) {
+const getRollbackMessage = function (price) {
   switch (true) {
     case price >= 30000:
       return "Даем скидку в 10%";
@@ -33,16 +35,38 @@ const retRollbackMessage = function (price) {
   }
 }
 
+const getAllServicePrices = function (price1, price2) {
+  allServicePrices = Number(price1) + Number(price2);
+  return allServicePrices;
+}
+
+function getFullPrice(price, allprice) {
+  fullPrice = Number(price) + Number(allprice);
+  return fullPrice;
+}
+
+function getTitle(title) {
+  title = title.toLowerCase();
+  title = title[1].toUpperCase() + title.substring(2);
+  return title;
+}
+
+function getServicePercentPrices(price, percent) {
+  servicePercentPrice = price - (price * (percent / 100));
+  return servicePercentPrice;
+}
+
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
+getAllServicePrices(servicePrice1, servicePrice2)
+getFullPrice(screenPrice, allServicePrices);
+getTitle(title);
+getServicePercentPrices(fullPrice, rollback);
 
-console.log(retRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(servicePercentPrice);
-
-console.log('Стоимость разработки сайта ' + screenPrice + '₽');
+console.log(showTypeOf(title));
+console.log(showTypeOf(fullPrice));
+console.log(showTypeOf(adaptive));
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(getServicePercentPrices(fullPrice, rollback));
