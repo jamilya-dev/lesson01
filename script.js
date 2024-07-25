32a0b3819706eae33a07a46b2fc827a96d7a570a
jamilya-dev <omg.groups@yandex.ru>
2024-07-25 15:47:26 +0300

    урок 4 пройден

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 'use strict';
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать? пример: "Простые, Сложные, Интерактивные"');
-const screenPrice = prompt('Сколько будет стоить данная работа?');
-const answer = confirm("Нужен ли адаптив на сайте?")
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = prompt('Сколько это будет стоить?');
-const fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
-const servicePercentPrice = Math.ceil(fullPrice - rollback);
 
-switch (true) {
-  case fullPrice >= 30000:
-    console.log('Даем скидку в 10%');
-    break
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log('Даем скидку в 5%');
-    break
-  case fullPrice >= 0 && fullPrice < 15000:
-    console.log('Скидка не предусмотрена');
-    break
-  case fullPrice < 0:
-    console.log('Что то пошло не так');
-    break
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать? пример: "Простые, Сложные, Интерактивные"');
+let screenPrice = prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm("Нужен ли адаптив на сайте?")
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
+let rollback = 10
+let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+let servicePercentPrice = Math.ceil(fullPrice - (rollback / 100));
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
 }
 
+const retRollbackMessage = function (price) {
+  switch (true) {
+    case price >= 30000:
+      return "Даем скидку в 10%";
+      break
+    case price >= 15000 && price < 30000:
+      return 'Даем скидку в 5%';
+      break
+    case price >= 0 && price < 15000:
+      return 'Скидка не предусмотрена';
+      break
+    case price < 0:
+      return 'Что то пошло не так';
+      break
+  }
+}
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(retRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
 console.log(servicePercentPrice);
+
+console.log('Стоимость разработки сайта ' + screenPrice + '₽');
