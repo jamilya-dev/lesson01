ebab95bcf70e695a0326a27216d33609c5af3b9f
jamilya-dev <omg.groups@yandex.ru>
2024-08-21 13:06:29 +0300

    работа над ошибками

--- script.js
+++ script.js
@@ -25,32 +25,37 @@ const formatTime = date.toLocaleString("en-US", {
   second: "numeric",
 });
 
-let days;
-let text;
+const createHello = (hours) => {
+  if (hours <= 5 || hours >= 21) {
+    return "Доброй ночи";
+  } else if (hours > 5 && hours <= 11) {
+    return "Доброe утро";
+  } else if (hours > 11 && hours <= 15) {
+    return "Добрый день";
+  } else if (hours > 15 && hours < 21) {
+    return "Добрый вечер";
+  }
+};
 
-if (hours <= 6 || hours >= 22) {
-  text = "Доброй ночи";
-} else if (hours > 6 && hours <= 12) {
-  text = "Доброe утро";
-} else if (hours > 12 && hours <= 16) {
-  text = "Добрый день";
-} else if (hours > 16 && hours < 22) {
-  text = "Добрый вечер";
-}
+const createDays = (day) => {
+  if (day.toString()[day.toString().length - 1] === "1") {
+    return " день";
+  } else if (
+    day.toString()[day.toString().length - 1] > "1" &&
+    day.toString()[day.toString().length - 1] < "5"
+  ) {
+    return " дня";
+  } else {
+    return " дней";
+  }
+};
 
-if (day.toString()[day.toString().length - 1] === 1) {
-  days = " день";
-} else if (
-  day.toString()[day.toString().length - 1] > 1 &&
-  day.toString()[day.toString().length - 1] < 5
-) {
-  days = " дня";
-} else {
-  days = " дней";
-}
+const createBlock = () => {
+  block.innerHTML = `${createHello(hours)},<br>
+	Сегодня ${dayWeek[weekDay - 1]},<br>
+	Текущее время ${formatTime},<br>
+	До нового года осталось ${day} ${createDays(day)}`;
+  document.body.append(block);
+};
 
-block.innerHTML = `${text},<br>
-Сегодня ${dayWeek[weekDay - 1]},<br>
-Текущее время ${formatTime},<br>
-До нового года осталось ${day} ${days}`;
-document.body.append(block);
+createBlock();
